4788cd27db369d6abe6eee7b22bfc4d8f7fe68d5
Arthur Silva <arthur2335silva@gmail.com>
2026-04-08 18:54:27 -0400

    starting assessment with bare bones env

---
+++ src/main.tsx
@@ -0,0 +1,8 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <h1>Hey now this is a test assessment</h1>
+  </StrictMode>,
+);
